b23ec6b88112297d9787c604a286b63b829269b3
loooolBean <BeanLiao@163.com>
2022-02-25 21:02:42 +0800

    Initial commit

--- mydjango/HelloWorld/HelloWorld/urls.py
+++ mydjango/HelloWorld/HelloWorld/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from . import views, testdb
+from . import views, testdb, search,searches
 
-# 拼接url地址
+# 拼接url地址,文件名.函数名
 urlpatterns = [
     path('runoob/', views.runoob),
-    path('testdb/', testdb.testdb)
+    path('testdb/', testdb.testdb),
+    path('search-form/', search.search_form),
+    path('search/', search.search),
+    path('search-post/', searches.search_post),
 ]
