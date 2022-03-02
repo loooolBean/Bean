d183c3b9eb94d0a2e86b783849076fd663c179ae
loooolBean <BeanLiao@163.com>
2022-03-02 14:45:22 +0800

    Initial commit

--- mydjango/HelloWorld/HelloWorld/urls.py
+++ mydjango/HelloWorld/HelloWorld/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from . import views, testdb, search,searches
+from . import views, testdb, search, searches
 from django.conf.urls import url
 from django.contrib import admin
 
 # 拼接url地址,文件名.函数名
 urlpatterns = [
-    url(r'^admin/', admin.site.urls), # 管理工具
+    path('add_book/', views.add_book),
+    url('admin/', admin.site.urls),  # 管理工具
     path('runoob/', views.runoob),
     path('testdb/', testdb.testdb),
     path('search-form/', search.search_form),
--- mydjango/HelloWorld/HelloWorld/views.py
+++ mydjango/HelloWorld/HelloWorld/views.py
@@ -1,16 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from TestModel import models
 
 
 def runoob(request):
     views_str = "<a href='https://www.runoob.com/'>点击跳转</a>"
     views_for = {"name": "菜鸟教程",
                  "gender": "nan"}
-    name="Bean"
+    name = "Bean"
     # 返回runoob的views_str、views_for、name参数
     return render(request, "runoob.html", {"views_str": views_str,
-                                           "views_for":views_for ,
-                                           "name":name})
+                                           "views_for": views_for,
+                                           "name": name})
+
 
 # def hello(request):
 #     return HttpResponse("Hello world ! ")
+
+# 方法一：模型类实例化对象
+# def add_book(request):
+#     book = models.Book(title="菜鸟教程",price=300,publish="菜鸟出版社",pub_date="2008-8-8")
+#     book.save()
+#     return HttpResponse("<p>数据添加成功！</p>")
+
+# 方法二：通过ORM提供的objects提供的方法create来实现（推荐）
+def add_book(request):
+    # book = models.Book(title="菜鸟教程",price=300,publish="菜鸟出版社",pub_date="2008-8-8")
+    # book.save()
+    # return HttpResponse("<p>数据添加成功！</p>")
+
+    #  获取出版社对象
+    pub_obj = models.Publish.objects.filter(pk=1).first()
+    #  给书籍的出版社属性publish传出版社对象
+    book = models.Book.objects.create(title="菜鸟教程", price=200, pub_date="2010-10-10", publish=pub_obj)
+    print(book, type(book))
+    return HttpResponse(book)
